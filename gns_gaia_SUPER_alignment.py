2071cf916cfdbb26f86a0d699e8f4f71b90e2e48
Alvaro Martinez <amartinez@iaa.es>
2025-03-29 12:39:45 +0100

    Making the alignment in Galactoc coordinates

--- gns_gaia_SUPER_alignment.py
+++ gns_gaia_SUPER_alignment.py
@@ -44,6 +44,8 @@ from filters import filter_vvv_data
 import Polywarp as pw
 from alignator import alignator
 from plot_machine import diff_hist
+from scipy import stats
+from matplotlib import colors 
 # %%plotting parametres
 from matplotlib import rc
 from matplotlib import rcParams
@@ -74,11 +76,75 @@ IPython.get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 
-field_one = 100
+field_one = 60
 chip_one = 0
 GNS_1='/Users/amartinez/Desktop/PhD/HAWK/GNS_1/lists/%s/chip%s/'%(field_one, chip_one)
 
 gns1=  Table.read(GNS_1 + 'stars_calibrated_H_chip%s.ecsv'%(chip_one),  format = 'ascii.ecsv')
+factor = -1
+pix_scale =0.5*0.1064
+p_mas = pix_scale*1000#Trnasform pixeles into mas
+gns1['x'] = gns1['x']*factor*p_mas
+gns1['y'] = gns1['y']*p_mas
+
+min_exp = gns1['nexp'] > np.max(gns1['nexp'])*0.25
+gns1 = gns1[min_exp]
+
+num_bins = 200
+s_xy = np.sqrt(gns1['sl']**2 + gns1['sb']**2)
+
+# Create a meshgrid for plotting
+
+# %
+# Plot the result
+fig, (ax,ax2) = plt.subplots(2,1, figsize =(6,6))
+statistic, x_edges, y_edges, binnumber = stats.binned_statistic_2d(gns1['l'], gns1['b'], s_xy, statistic='median', bins=(num_bins,int(num_bins/2)))
+X, Y = np.meshgrid(x_edges, y_edges)
+
+# c = ax.pcolormesh(X, Y, statistic.T, cmap='Spectral',norm = colors.LogNorm())
+c = ax.pcolormesh(X, Y, statistic.T, cmap='Spectral_r', vmin = 0.01,vmax = 0.05)
+cb1 = fig.colorbar(c, ax=ax, label=r'$\sqrt{sl^2 +sb^2}$ [arcsec]', shrink = 0.8,)
+# ax.colorbar(c, ax=ax, label=r'$\sqrt{sl^2 +sb^2}$ [arcsec]', shrink = 0.5)
+ax.set_xlabel('l(º)')
+ax.set_ylabel('b (º)')
+ax.axis('scaled')
+
+statistic, x_edges, y_edges, binnumber = stats.binned_statistic_2d(gns1['x'], gns1['y'], s_xy, statistic='median', bins=(num_bins,int(num_bins/2)))
+X, Y = np.meshgrid(x_edges, y_edges)
+
+c2 = ax2.pcolormesh(X, Y, statistic.T, cmap='Spectral_r',vmin = 0.01,vmax = 0.05)
+cb2  = fig.colorbar(c2, ax=ax2, label=r'$\sqrt{sl^2 +sb^2}$ [arcsec]', shrink = 0.8, )
+# ax2.colorbar(c2, ax=ax, label=r'$\sqrt{sl^2 +sb^2}$ [arcsec]', shrink = 0.5)
+ax2.set_xlabel('x [mas]')
+ax2.set_ylabel('y[mas]')
+ax2.axis('scaled')
+
+
+
+fig,ax = plt.subplots(1,1)
+N=1
+# ax.scatter(gns1['x'][::N],gns1['y'][::N])
+ax.scatter(gns1['l'][::N],gns1['b'][::N])
+
+mx = (np.min(gns1['x']) + np.max(gns1['x']))/2
+my = (np.min(gns1['y']) + np.max(gns1['y']))/2
+
+chip1 = (gns1['x'] > np.min(gns1['x'])) & (gns1['x'] < mx*1.1) & (gns1['y'] >0) & (gns1['y'] <my*0.9) 
+chip2 = (gns1['x'] > mx*0.95) & (gns1['x'] < mx*-2) & (gns1['y'] >0) & (gns1['y'] <my*0.9) 
+chip3 = (gns1['x'] > mx*0.95) & (gns1['x'] < mx*-2) & (gns1['y'] >my*1.1) & (gns1['y'] <my*2) 
+chip4 = (gns1['x'] > np.min(gns1['x']))  & (gns1['x'] < mx*1.1) & (gns1['y'] >my*1.1) & (gns1['y'] <my*2) 
+
+all_chips= chip1 | chip2 | chip3 | chip4
+# all_chips= chip2 | chip3
+
+# gns1 = gns1[chip1]
+gns1 = gns1[all_chips]
+# gns1 = gns1[np.logical_not(all_chips)]
+
+
+# ax.scatter(gns1['x'][::N],gns1['y'][::N],s =3)
+ax.scatter(gns1['l'][::N],gns1['b'][::N],s =3)
+ax.axis('equal')
 
 radec_ = SkyCoord(l = gns1['l'], b = gns1['b'], frame = 'galactic', ).fk5
 Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source" # Select early Data Release 3
@@ -87,14 +153,21 @@ Gaia.ROW_LIMIT = -1  # Ensure the default row limit.
 coord = SkyCoord(ra=np.mean(radec_.ra), dec=np.mean(radec_.dec), unit='degree', frame='icrs',equinox ='J2000',obstime='J2016.0')
 
 
+transf = 'affine'#!!!
+# transf = 'similarity'#!!!1
+# transf = 'polynomial'#!!!
 
+order_trans = 1
+e_pm = 0.3#!!!
+max_sep = 0.1*u.arcsec#!!!Frist Macthig distance with Gaia 
+d_m = 200 #!!! distance in mas using for compare_lists
+max_deg = 3#!!! Maximun degree using for the interative polynomial alignment
+clip_in_alig = 'yes'#!!! Perform clipping inside the interative polynomial alignment
+bad_sig  = 3
+align_1 = 'Polywarp'
+# align_1 = '2DPoly'
+f_mode = 'NWnC'
 
-e_pm = 0.3
-
-# j = Gaia.cone_search_async(coord, )
-# coord = SkyCoord(ra=10.625, dec=41.2, unit=(u.degree, u.degree), frame='icrs')
-# j = Gaia.cone_search_async(coord,100*u.arcsec)
-# j = Gaia.cone_search_async(coord,100*u.arcsec)
 size = 0.35*u.deg
 gaia= Gaia.query_object_async(coord, width= 0.5*size, height=size)
 # gaia_ = Gaia.cone_search((coord.ra, coord.dec),0.5*size)
@@ -118,7 +191,13 @@ gaia_good = filter_gaia_data(
 
 gaia = gaia_good
 
-gaia_coord = SkyCoord(l = gaia['l'].value*u.deg, b = gaia['b'].value*u.deg, frame = 'galactic')
+gaia_coord = SkyCoord(l = gaia['l'], b = gaia['b'], frame = 'galactic')
+coord = coord.transform_to('galactic')
+ragai1_off,decgai1_off = coord.spherical_offsets_to(gaia_coord.frame)
+
+gaia['x'] = ragai1_off.to(u.mas)
+gaia['y'] = decgai1_off.to(u.mas)
+
 
 lw_g = gaia_coord.l.wrap_at('361d').degree
 # lw_g = gaia_coord.l.wrap_at(360)
@@ -136,11 +215,11 @@ ax.scatter(gaia['l'],gaia['b'], label =f'Gaia = {len(gaia)}')
 ax.axis('scaled')
 
 
-max_sep = 0.5*u.arcsec
+
 ga_c = SkyCoord(l = gaia['l'], b = gaia['b'], unit = 'degree',frame = 'galactic' )
 gn_c= SkyCoord(l= gns1['l'], b=gns1['b'],unit = 'degree', frame = 'galactic')
 # gns1_coor_match = gns1_coor_fg.transform_to('icrs')
-idx,d2d,d3d = ga_c.match_to_catalog_sky(gn_c,nthneighbor=1)# ,nthneighbor=1 is for 1-to-1 match
+idx,d2d,d3d = ga_c.match_to_catalog_sky(gn_c)# ,nthneighbor=1 is for 1-to-1 match
 sep_constraint = d2d < max_sep
 ga_m = gaia[sep_constraint]
 gn_m = gns1[idx[sep_constraint]]
@@ -162,7 +241,7 @@ diff_lc= diff_l[mask_lb]
 diff_bc= diff_b[mask_lb]
 # %%
 fig, (ax, ax2) = plt.subplots(1,2)
-
+ax.set_title(f'Matches = {len(diff_lc)}')
 ax.hist(diff_lc, histtype = 'step',linewidth =1, label = fr'$\Delta$ l = {np.mean(diff_lc): .2f}$\pm$ {np.std(diff_lc): .2f} arcsec' )
 ax2.hist(diff_bc, histtype = 'step',linewidth =1,label = fr'$\Delta$ b = {np.mean(diff_bc): .2f}$\pm$ {np.std(diff_bc): .2f} arcsec')
 ax.hist(diff_l,color = 'k', alpha = 0.05)
@@ -179,30 +258,60 @@ ax2.set_xlabel(r'$\Delta b$[arcsec]')
 # %%
 
 
-gn_m = gn_m[mask_lb]
-ga_m = ga_m[mask_lb]
+# gn_m = gn_m[mask_lb]
+# ga_m = ga_m[mask_lb]
 
 
-radec_ = radec_.transform_to('icrs')
-ragai1_off,decgai1_off = radec_.spherical_offsets_to(GaiaCoord.frame)
+# =============================================================================
+# xy_gn = np.array([gn_m['x'],gn_m['y']]).T
+# xy_ga = np.array([ga_m['x'],ga_m['y']]).T
+# =============================================================================
+xy_gn = np.array([gn_m['l'],gn_m['b']]).T
+xy_ga = np.array([ga_m['l'],ga_m['b']]).T
 
-xy_gn = np.array([gn_m['x'],gn_m['y']]).T
-xy_ga = np.array([ga_m['x'],ga_m['y']]).T
-
-
-
-
-
-
-
-
-
-
+# %%
+fig, ax = plt.subplots(1,1)
+ax.scatter(xy_gn[:,0],xy_gn[:,1])
+ax.scatter(xy_ga[:,0],xy_ga[:,1],s = 1)
+ax.axis('equal')
 
 
 
+if transf == 'polynomial':
+    p = ski.transform.estimate_transform(transf,
+                                        xy_gn, 
+                                        xy_ga, order = order_trans)
+else:    
+    p = ski.transform.estimate_transform(transf,
+                                    xy_gn, 
+                                    xy_ga)
 
 
+
+print(p)
+
+# %%
+fig, ax = plt.subplots(1,1)
+# ax.scatter(gn_m['x'],gn_m['y'], marker = '*', label = 'GNS1')
+# ax.scatter(ga_m['x'],ga_m['y'], marker = '*', label = 'Gaia')
+ax.scatter(gn_m['l'],gn_m['b'], marker = '*', label = 'GNS1')
+ax.scatter(ga_m['l'],ga_m['b'], marker = '*', label = 'Gaia')
+xy_gn_t = p(xy_gn)
+ax.scatter(xy_gn_t[:,0],xy_gn_t[:,1], label = 'GNS1_t',s=2)
+ax.legend()
+gn_xy = np.array([gns1['x'],gns1['y']]).T
+gn_xyt = p(gn_xy)
+gns1['x'] = gn_xyt[:,0] 
+gns1['y'] = gn_xyt[:,1] 
+gns1['x'] = gn_xyt[:,0] 
+gns1['y'] = gn_xyt[:,1] 
+ax.axis('equal')
+
+
+sys.exit(278)
+s_ls = compare_lists(np.array([gns1['x'],gns1['y']]).T, np.array([gaia['x'],gaia['y']]).T, d_m)
+# %%
+gns1 = alignator(1, gns1, gaia, s_ls, d_m, max_deg =max_deg, clipping = clip_in_alig, align_by = align_1, f_mode = f_mode, plot = 'yes')
 
 
 
